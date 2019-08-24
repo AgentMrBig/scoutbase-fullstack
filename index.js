86882f2d8ba4c4b17300b9e41620718981e050b9
Eric Hendrix <agentmrbig@gmail.com>
2019-08-24 00:39:17 -0400

    clean react app

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors());
+
+const PORT = 4000;
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
--- readme.md
+++ readme.md
@@ -1,5 +1,3 @@
-Code challenge for Scoutbase
-
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
